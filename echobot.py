af91c955b89c955dac6526cd67468516d55cdbe4
Imran Alaz <imran.alaz@cartesius2.nl>
2019-07-04 09:53:53 +0200

    New things

--- echobot.py
+++ echobot.py
@@ -16,12 +16,12 @@ def pattern(phrase):
         for pattern in patterns:
                 match = re.match(pattern['pattern'].lower(), phrase.lower())
                 if match:
-                        return pattern['response'].format(match.group(1))
+                        return random.choice(pattern['responses']).format(match.group(1))
 def swap_pronouns(phrase):
-        if 'I' in phrase:
-                return re.sub('I', 'your', phrase)
-        if 'my' in phrase:
-                return  re.sub('my', 'your', phrase)
+        if 'I ' in phrase:
+                return re.sub('am', 'are', re.sub('I', 'you', phrase))
+        if 'my ' in phrase:
+                return  re.sub('my', 'you', re.sub('my', 'you', phrase))
         else: 
                 return phrase
 
@@ -29,9 +29,9 @@ def swap_pronouns(phrase):
 def start():
     load_json()
     global name
-    name = raw_input("{} Mijn naam is E.C.H.O, wat is jouw naam?\n".format("E.C.H.O: "))
+    name = raw_input("{} My name is E.C.H.O, what is your name?\n".format("E.C.H.O: "))
     
-    print("{}: Welkom, {} bij E.C.H.O.".format("E.C.H.O: ", name))
+    print("{}: Welcome, {} at E.C.H.O.".format("E.C.H.O: ", name))
 
     while True:
         listen()
@@ -41,7 +41,6 @@ def listen():
     user_input = raw_input()
     reply = get_reply(user_input)
     if reply is None:
-        #TODO: Fix this :)
         reply = random.choice(random_responses)
     print_user_and_bot(name, "E.C.H.O", user_input, reply)
 
@@ -53,11 +52,20 @@ def get_reply(question):
             return pattern(question)
 
     for response in responses:
-            for keyword in response['keywords']:
-                    if keyword in question.lower():
-                            if swap_pronouns(response['response']):
-                                    return swap_pronouns(response['response'])
+            keywordAmount = 0
+            if question.lower() in response['question'].lower():
+                    if swap_pronouns(response['response']):
+                            return swap_pronouns(response['response'])
+                    else:
                             return response['response']
+        #     for  in response['keywords']:
+        #             if keyword in question.lower():
+        #                     if keywordAmount == len(response['keywords']):
+        #                         if swap_pronouns(response['response']):
+        #                                 return swap_pronouns(response['response'])
+        #                         return response['response']
+#     if swap_pronouns(question):
+#            return swap_pronouns(question)
 
 def print_user_and_bot(user, bot, message, bot_reply):
     print("{}: {}".format(user, message))
